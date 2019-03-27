7f4ed1fe442715b97fe5f5e07d80d3f6226159a6
leonardotanzi <leo.tanzi@gmail.com>
2019-03-27 12:44:52 +0100

    Orb detector

--- Source/MatchFeature/BoneDetectionFeature.py
+++ Source/MatchFeature/BoneDetectionFeature.py
@@ -4,7 +4,33 @@ import numpy as np
 from PIL import Image
 from matplotlib import pyplot as plt
 
+img1 = cv2.imread("/Users/leonardotanzi/Desktop/TesiMagistrale/Audisio/ImmaginiBacinoCopiaCrop/kitchen.jpg", 0)          # queryImage
+img2 = cv2.imread("/Users/leonardotanzi/Desktop/MasterThesis/Templates/fork.jpg", 0) # trainImage
 
+
+# Initiate ORB detector
+orb = cv2.ORB_create()
+
+# find the keypoints with ORB
+kp1 = orb.detect(img1, None)
+kp2 = orb.detect(img2, None)
+# compute the descriptors with ORB
+kp1, des1 = orb.compute(img1, kp1)
+kp2, des2 = orb.compute(img2, kp2)
+
+# create BFMatcher object
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+# Match descriptors.
+matches = bf.match(des1, des2)
+# Sort them in the order of their distance.
+matches = sorted(matches, key=lambda x: x.distance)
+# Draw first 10 matches.
+img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches[:10], None, flags=2)
+plt.imshow(img3)
+plt.show()
+
+
+'''
 def find_bones(input_image, template_name, output_path, i):
 
     methods = [  # 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR_NORMED',
@@ -87,3 +113,4 @@ if __name__ == "__main__":
             i += 1
             if i == 10:
                 break
+'''
