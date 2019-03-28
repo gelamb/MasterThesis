4e36816997e832e0bb6b055d7d6013f596707945
leonardotanzi <leo.tanzi@gmail.com>
2019-03-28 17:17:43 +0100

    Multi scale, rotation + weight

--- Source/MatchTemplate/BoneDetectionMultiScale.py
+++ Source/MatchTemplate/BoneDetectionMultiScale.py
@@ -3,18 +3,18 @@ import numpy as np
 import imutils
 import glob
 import cv2
-import crop_rotate
+import crop_rotate as cr
 
 
 # load the image image, convert it to grayscale, and detect edges
 template = cv2.imread("/Users/leonardotanzi/Desktop/MasterThesis/Templates/Bacino_left.jpg")
 template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-template = cv2.Canny(template, 50, 100, 7)
+template = cv2.Canny(template, 50, 200, 7)
 (tH, tW) = template.shape[:2]
-template2 = cv2.resize(template, (400, 400))
-cv2.imshow("Template", template2)
-v = True
-using_min = False
+# template2 = cv2.resize(template, (400, 400))
+# cv2.imshow("Template", template2)
+v = False
+using_min = True  # work better with true
 
 # loop over the images to find the template in
 for imagePath in glob.glob("/Users/leonardotanzi/Desktop/TesiMagistrale/Audisio/ImmaginiBacinoCopiaCrop/*.jpg"):
@@ -25,7 +25,7 @@ for imagePath in glob.glob("/Users/leonardotanzi/Desktop/TesiMagistrale/Audisio/
     found = None
 
     # loop over the scales of the image
-    for scale in np.linspace(0.8, 1.0, 10)[::-1]: # linspace(start, stop, num of samples to gen) the - 1 reverse the
+    for scale in np.linspace(0.9, 1.1, 5)[::-1]: # linspace(start, stop, num of samples to gen) the - 1 reverse the
                                                 #  list So, when you do a[::-1] , it starts from the end, towards the
                                                 #  first, taking each element. So it reverses a
         # resize the image according to the scale, and keep track
@@ -38,40 +38,43 @@ for imagePath in glob.glob("/Users/leonardotanzi/Desktop/TesiMagistrale/Audisio/
         if resized.shape[0] < tH or resized.shape[1] < tW:
             break
 
-        # detect edges in the resized, grayscale image and apply template
-        # matching to find the template in the image
-        edged = cv2.Canny(resized, 50, 100)
+        images = [resized, cr.crop_rotate(resized, 5), cr.crop_rotate(resized, -5)]
 
-        if using_min:
-            result = cv2.matchTemplate(edged, template, cv2.TM_SQDIFF)
-            val, y, loc, _ = cv2.minMaxLoc(result)
-        else:
-            result = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF)
-            y, val, _, loc = cv2.minMaxLoc(result)
+        for img in images:
+            # detect edges in the resized, grayscale image and apply template
+            # matching to find the template in the image
+            edged = cv2.Canny(img, 50, 200, 7)
 
-        # check to see if the iteration should be visualized
-        if v:
-            # draw a bounding box around the detected region
-            clone = np.dstack([edged, edged, edged])
-            cv2.rectangle(clone, (loc[0], loc[1]), (loc[0] + tW, loc[1] + tH), (0, 0, 255), 30)
-            cv2.imshow("Visualize", cv2.resize(clone, (500, 500)))
-            cv2.waitKey(0)
+            if using_min:
+                result = cv2.matchTemplate(edged, template, cv2.TM_SQDIFF)
+                val, _, loc, _ = cv2.minMaxLoc(result)
+            else:
+                result = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF)
+                _, val, _, loc = cv2.minMaxLoc(result)
 
-        # if we have found a new maximum correlation value, then update
-        # the bookkeeping variable
-        if (found is None or val < found[0]) and using_min is True:
-            found = (val, loc, r)
-        elif (found is None or val > found[0]) and using_min is False:
-            found = (val, loc, r)
+            # check to see if the iteration should be visualized
+            if v:
+                # draw a bounding box around the detected region
+                clone = np.dstack([edged, edged, edged])
+                cv2.rectangle(clone, (loc[0], loc[1]), (loc[0] + tW, loc[1] + tH), (0, 0, 255), 30)
+                cv2.imshow("Visualize", cv2.resize(clone, (500, 500)))
+                cv2.waitKey(0)
 
-        print("Value is {} and y {}".format(val, y))
+            # if we have found a new maximum correlation value, then update
+            # the bookkeeping variable
+            # weighted the val, if left more weighted
+            if (found is None or val * loc[0] < found[0]) and using_min is True:
+                found = (val, loc, r)
+            elif (found is None or val / (loc[0] if loc[0] != 0 else 0.1)) and using_min is False:
+                found = (val, loc, r)
+
+            # print("Value is {}".format(val))
+            
     # unpack the bookkeeping variable and compute the (x, y) coordinates
     # of the bounding box based on the resized ratio
     (_, loc, r) = found
     (startX, startY) = (int(loc[0] * r), int(loc[1] * r))
     (endX, endY) = (int((loc[0] + tW) * r), int((loc[1] + tH) * r))
-
-    print("Minimum value is {}".format(found[0]))
 
     # draw a bounding box around the detected result and display the image
     cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 30)
--- Source/MatchTemplate/crop_rotate.py
+++ Source/MatchTemplate/crop_rotate.py
@@ -1,58 +1,58 @@
 import cv2
 import numpy as np
 
+
 def resize(img, scale_percent):
-	
+
 	width = int(img.shape[1] * scale_percent / 100)
 	height = int(img.shape[0] * scale_percent / 100)
 	dim = (width, height)
-	resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+	resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
 	return resized
- 
+
+
 def rotate_bound(image, angle):
-    # grab the dimensions of the image and then determine the
-    # center
-    (h, w) = image.shape[:2]
-    (cX, cY) = (w // 2, h // 2)
- 
-    # grab the rotation matrix (applying the negative of the
-    # angle to rotate clockwise), then grab the sine and cosine
-    # (i.e., the rotation components of the matrix)
-    M = cv2.getRotationMatrix2D((cX, cY), -angle, 1.0)
-    cos = np.abs(M[0, 0])
-    sin = np.abs(M[0, 1])
- 
-    # compute the new bounding dimensions of the image
-    nW = int((h * sin) + (w * cos))
-    nH = int((h * cos) + (w * sin))
- 
-    # adjust the rotation matrix to take into account translation
-    M[0, 2] += (nW / 2) - cX
-    M[1, 2] += (nH / 2) - cY
- 
-    # perform the actual rotation and return the image
-    rotated = cv2.warpAffine(image, M, (nW, nH))
-    return rotated
+	# grab the dimensions of the image and then determine the center
+	(h, w) = image.shape[:2]
+	(cX, cY) = (w // 2, h // 2)
+
+	# grab the rotation matrix (applying the negative of the
+	# angle to rotate clockwise), then grab the sine and cosine
+	# (i.e., the rotation components of the matrix)
+	M = cv2.getRotationMatrix2D((cX, cY), -angle, 1.0)
+	cos = np.abs(M[0, 0])
+	sin = np.abs(M[0, 1])
+
+	# compute the new bounding dimensions of the image
+	nW = int((h * sin) + (w * cos))
+	nH = int((h * cos) + (w * sin))
+
+	# adjust the rotation matrix to take into account translation
+	M[0, 2] += (nW / 2) - cX
+	M[1, 2] += (nH / 2) - cY
+
+	# perform the actual rotation and return the image
+	rotated = cv2.warpAffine(image, M, (nW, nH))
+	return rotated
+
 
 def crop_rotate(image, degree):
 
 	(h, w) = image.shape[:2]
-
-	rotated = rotate_bound(image, 10) 
+	rotated = rotate_bound(image, degree)
 	(h_r, w_r) = rotated.shape[:2]
 	diff_h = h_r - h
 	diff_w = w_r - w
-	print("h is {} w is {} h_r is {} w_r is {}".format(h, w, h_r, w_r))
-
 	cropped = rotated[diff_h:-diff_h, diff_w:-diff_w]
-
-	cv2.imshow("original", resize(image, 50))
+	return cropped
+	'''
+	cv2.imshow("original", resize(image, 30))
 	cv2.waitKey(0)
-	cv2.imshow("rotate", resize(rotated, 50))
+	cv2.imshow("rotate", resize(rotated, 30))
 	cv2.waitKey(0)
-	cv2.imshow("crop", resize(cropped, 50))
+	cv2.imshow("crop", resize(cropped, 30))
 	cv2.waitKey(0)
+	'''
 
-
-#image = cv2.imread("/Users/leonardotanzi/Desktop/MasterThesis/Templates/Bacino_left.jpg")
-#crop_rotate(image, 10)
+# image = cv2.imread("/Users/leonardotanzi/Desktop/MasterThesis/Templates/Bacino_left.jpg")
+# crop_rotate(image, 10)
