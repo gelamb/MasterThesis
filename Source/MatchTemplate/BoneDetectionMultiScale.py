32a8baddd5d75eb59f94779eafad73eb5a5123e1
leonardotanzi <leo.tanzi@gmail.com>
2019-04-01 11:39:16 +0200

    Added alternative approach

--- Source/MatchTemplate/BoneDetectionMultiScale.py
+++ Source/MatchTemplate/BoneDetectionMultiScale.py
@@ -4,6 +4,7 @@ import imutils
 import glob
 import cv2
 import crop_rotate as cr
+from PIL import Image
 
 
 def pre_process_image(image):
@@ -46,10 +47,11 @@ if __name__ == "__main__":
     scale_index = 0 if using_scale is 1 else 0.1
     using_weights = True
     save_images = False
-    alternative_approach = False
+    alternative_approach = True
     # text for images
     templates_text = ["Normal", "Flipped"]
     rotation_text = ["Normal", "Right", "Left"]
+    output_path = "/Users/leonardotanzi/Desktop/MasterThesis/Output/"
     # canny threshold values
 
     # load the image image, convert it to grayscale, and detect edges
@@ -66,106 +68,136 @@ if __name__ == "__main__":
     # loop over the images to find the template in
     for imagePath in glob.glob("/Users/leonardotanzi/Desktop/TesiMagistrale/Audisio/ImmaginiBacinoCopiaCrop/*.jpg"):
 
-        for i in range(2):
-
+        if alternative_approach:
+            distance_from_border = 60
             image = cv2.imread(imagePath)
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            found = None
+            edged = pre_process_image(image)
+            (h, w) = edged.shape[:2]
 
-            # loop over the scales of the image
-            for scale in np.linspace(1 - scale_index, 1 + scale_index, using_scale)[::-1]:
-                # linspace(start, stop, num of samples to gen) the - 1 reverse the list So, when you do a[::-1] , it
-                # starts from the end, towards the first, taking each element. So it reverses a resize the image
-                # according to the scale, and keep track of the ratio of the resizing
-                resized = imutils.resize(gray, width=int(gray.shape[1] * scale))
-                r = gray.shape[1] / float(resized.shape[1])
+            if using_min:
+                result = cv2.matchTemplate(edged, templates[0], cv2.TM_SQDIFF)
+                val, _, loc, _ = cv2.minMaxLoc(result)
+            else:
+                result = cv2.matchTemplate(edged, templates[0], cv2.TM_CCOEFF)
+                _, val, _, loc = cv2.minMaxLoc(result)
+
+            clone = np.dstack([edged, edged, edged])
+            # cv2.rectangle(clone, (loc[0], loc[1]), (loc[0] + tW, loc[1] + tH), (0, 0, 255), 30)
+            # cv2.circle(clone, (loc[0], loc[1]), 30, (0, 255, 0), thickness=-1)
+            box_left = (0 + distance_from_border, loc[1] - int(tH/4), loc[0] + int(tW/4), h - distance_from_border)
+            box_right = (loc[0] + int(3/4*tW), loc[1] - int(tH/4), w - distance_from_border, h - distance_from_border)
+            cv2.rectangle(clone, (box_left[0], box_left[1]), (box_left[2], box_left[3]), (0, 0, 255), 10)
+            cv2.rectangle(clone, (box_right[0], box_right[1]), (box_right[2], box_right[3]), (0, 0, 255), 10)
+            if v:
+                print_img(imagePath.split("/")[-1], clone)
+            image_to_crop = Image.open(imagePath)
+            crop_image_left = image_to_crop.crop(box_left)
+            crop_image_right = image_to_crop.crop(box_right)
+
+            crop_image_left.save(output_path + ("{}_left.jpg".format(imagePath.split("/")[-1].split(".")[-2])))
+            crop_image_right.save(output_path + ("{}_right.jpg".format(imagePath.split("/")[-1].split(".")[-2])))
 
 
-                # if the resized image is smaller than the template, then break from the loop
-                if resized.shape[0] < tH or resized.shape[1] < tW:
-                    break
+        else:
+            for i in range(2):
 
-                if using_rotation:
-                    # list with original image, image rotated 5 degrees and image rotated -5 degrees
-                    images = [resized, cr.crop_rotate(resized, 5), cr.crop_rotate(resized, -5)]
-                else:
-                    images = [resized]
+                image = cv2.imread(imagePath)
+                gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                found = None
 
-                j = 0
-                for img in images:
-                    # just for the output text
-                    if using_rotation is False:
-                        j = 0
-                    # detect edges in the resized, grayscale image and apply template
-                    # matching to find the template in the image
-                    edged = pre_process_image(img)
+                # loop over the scales of the image
+                for scale in np.linspace(1 - scale_index, 1 + scale_index, using_scale)[::-1]:
+                    # linspace(start, stop, num of samples to gen) the - 1 reverse the list So, when you do a[::-1] , it
+                    # starts from the end, towards the first, taking each element. So it reverses a resize the image
+                    # according to the scale, and keep track of the ratio of the resizing
+                    resized = imutils.resize(gray, width=int(gray.shape[1] * scale))
+                    r = gray.shape[1] / float(resized.shape[1])
 
-                    if using_min:
-                        result = cv2.matchTemplate(edged, templates[i], cv2.TM_SQDIFF)
-                        val, _, loc, _ = cv2.minMaxLoc(result)
+                    # if the resized image is smaller than the template, then break from the loop
+                    if resized.shape[0] < tH or resized.shape[1] < tW:
+                        break
+
+                    if using_rotation:
+                        # list with original image, image rotated 5 degrees and image rotated -5 degrees
+                        images = [resized, cr.crop_rotate(resized, 5), cr.crop_rotate(resized, -5)]
                     else:
-                        result = cv2.matchTemplate(edged, templates[i], cv2.TM_CCOEFF)
-                        _, val, _, loc = cv2.minMaxLoc(result)
+                        images = [resized]
 
-                    # check to see if the iteration should be visualized
-                    if v:
-                        # print_img("t1", templates[i], save=False)
-                        window_name = "Image {}, Template {}, Scale {}, Rotation {}".format(imagePath.split("/")[-1],
-                                                                                         templates_text[i],
-                                                                                         scale,
-                                                                                         rotation_text[j])
-                        # draw a bounding box around the detected region
-                        clone = np.dstack([edged, edged, edged])
-                        cv2.rectangle(clone, (loc[0], loc[1]), (loc[0] + tW, loc[1] + tH), (0, 0, 255), 30)
-                        print_img(window_name, clone)
+                    j = 0
+                    for img in images:
+                        # just for the output text
+                        if using_rotation is False:
+                            j = 0
+                        # detect edges in the resized, grayscale image and apply template
+                        # matching to find the template in the image
+                        edged = pre_process_image(img)
 
-                    # if we have found a new maximum/minimum correlation value, then update the bookkeeping variable
-                    # weight the val if left or right
-                    if using_weights:
-                        mult = val * loc[0]
-                        div = val / (loc[0] if loc[0] != 0 else 0.01)
+                        if using_min:
+                            result = cv2.matchTemplate(edged, templates[i], cv2.TM_SQDIFF)
+                            val, _, loc, _ = cv2.minMaxLoc(result)
+                        else:
+                            result = cv2.matchTemplate(edged, templates[i], cv2.TM_CCOEFF)
+                            _, val, _, loc = cv2.minMaxLoc(result)
 
-                        if i == 0:  # left
-                            # update the min/max if it's the first cycle or if the weighted val is less/more than the
-                            # previous weight the val using the position, in the left case, the more the point is at
-                            # left, the more is weighted (so if loc[0] -> 0 (left side) the value must increase,
-                            # if loc[0]->infinite (right side) the value must decrease
+                        # check to see if the iteration should be visualized
+                        if v:
+                            # print_img("t1", templates[i], save=False)
+                            window_name = "Image {}, Template {}, Scale {}, Rotation {}".format(imagePath.split("/")[-1],
+                                                                                             templates_text[i],
+                                                                                             scale,
+                                                                                             rotation_text[j])
+                            # draw a bounding box around the detected region
+                            clone = np.dstack([edged, edged, edged])
+                            cv2.rectangle(clone, (loc[0], loc[1]), (loc[0] + tW, loc[1] + tH), (0, 0, 255), 30)
+                            print_img(window_name, clone)
 
-                            # più è a sinistra, quindi piu loc[0] è piccolo, più è piccolo val * loc[0], perchè sto set min
-                            if (found is None or mult < found[0]) and using_min is True:
-                                found = (mult, loc, r)
-                            # più è a sinistra, quindi piu loc[0] è piccolo, più è grande val / loc[0], perchè sto set max
-                            elif (found is None or div > found[0]) and using_min is False:
-                                found = (div, loc, r)
-                        else:  # right
-                            # più è a destra, quindi piu loc[0] è grande, più è piccolo val / loc[0], perchè sto set min
-                            if (found is None or div < found[0]) and using_min is True:
-                                found = (div, loc, r)
-                            # più è a destra, quindi piu loc[0] è grande, più è grande val * loc[0], perchè sto set max
-                            elif (found is None or mult > found[0]) and using_min is False:
-                                found = (mult, loc, r)
-                    else:
-                        if i == 0:  # left
-                            if (found is None or val < found[0]) and using_min is True:
-                                found = (val, loc, r)
-                            elif (found is None or val > found[0]) and using_min is False:
-                                found = (val, loc, r)
-                        else:  # right
-                            if (found is None or val < found[0]) and using_min is True:
-                                found = (val, loc, r)
-                            elif (found is None or val > found[0]) and using_min is False:
-                                found = (val, loc, r)
+                        # if we have found a new maximum/minimum correlation value, then update the bookkeeping variable
+                        # weight the val if left or right
+                        if using_weights:
+                            mult = val * loc[0]
+                            div = val / (loc[0] if loc[0] != 0 else 0.01)
 
-                    j += 1
+                            if i == 0:  # left
+                                # update the min/max if it's the first cycle or if the weighted val is less/more than
+                                # the previous weight the val using the position, in the left case, the more the point
+                                # is atleft, the more is weighted (so if loc[0] -> 0 (left side) the value must increase
+                                # if loc[0]->infinite (right side) the value must decrease
 
-            # unpack the bookkeeping variable and compute the (x, y) coordinates
-            # of the bounding box based on the resized ratio
-            (_, loc, r) = found
-            (startX, startY) = (int(loc[0] * r), int(loc[1] * r))
-            (endX, endY) = (int((loc[0] + tW) * r), int((loc[1] + tH) * r))
+                                # più è a sinistra, quindi piu loc[0] è piccolo, più è piccolo val * loc[0], perchè sto set min
+                                if (found is None or mult < found[0]) and using_min is True:
+                                    found = (mult, loc, r)
+                                # più è a sinistra, quindi piu loc[0] è piccolo, più è grande val / loc[0], perchè sto set max
+                                elif (found is None or div > found[0]) and using_min is False:
+                                    found = (div, loc, r)
+                            else:  # right
+                                # più è a destra, quindi piu loc[0] è grande, più è piccolo val / loc[0], perchè sto set min
+                                if (found is None or div < found[0]) and using_min is True:
+                                    found = (div, loc, r)
+                                # più è a destra, quindi piu loc[0] è grande, più è grande val * loc[0], perchè sto set max
+                                elif (found is None or mult > found[0]) and using_min is False:
+                                    found = (mult, loc, r)
+                        else:
+                            if i == 0:  # left
+                                if (found is None or val < found[0]) and using_min is True:
+                                    found = (val, loc, r)
+                                elif (found is None or val > found[0]) and using_min is False:
+                                    found = (val, loc, r)
+                            else:  # right
+                                if (found is None or val < found[0]) and using_min is True:
+                                    found = (val, loc, r)
+                                elif (found is None or val > found[0]) and using_min is False:
+                                    found = (val, loc, r)
 
-            # draw a bounding box around the detected result and display the image
-            cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 30)
-            name = (imagePath.split("/")[-1]).split(".")[-2] + "_" + str(i)
-            print_img("TemplateDetect", image, save=True, save_name=name)
+                        j += 1
+
+                # unpack the bookkeeping variable and compute the (x, y) coordinates
+                # of the bounding box based on the resized ratio
+                (_, loc, r) = found
+                (startX, startY) = (int(loc[0] * r), int(loc[1] * r))
+                (endX, endY) = (int((loc[0] + tW) * r), int((loc[1] + tH) * r))
+
+                # draw a bounding box around the detected result and display the image
+                cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 30)
+                name = (imagePath.split("/")[-1]).split(".")[-2] + "_" + str(i)
+                print_img("TemplateDetect", image, save=True, save_name=name)
 
